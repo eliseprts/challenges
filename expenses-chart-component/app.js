0483109e0658f27239b810ffc1253c3cf0ff0ea5
eliseprts <elisepourtois.pro@gmail.com>
2022-07-04 12:04:21 +0200

    add footer, fetch data from json file, in chart : change bg color to highlight highest value

--- expenses-chart-component/app.js
+++ expenses-chart-component/app.js
@@ -1,62 +1,88 @@
 const ctx = document.getElementById("spendingChart").getContext("2d")
 
-// const labels = Utils.days({ count: 7 })
-const labels = [
-    "mon",
-    "tue",
-    "wed",
-    "thu",
-    "fri",
-    "sat",
-    "sun"
-]
+const labels = []
+const spending = []
+const backgroundcolor = []
+const hoverbackgroundcolor = []
 
-const data = {
-    labels: labels,
-    datasets: [{
-        data: [65, 59, 80, 81, 56, 55, 40],
-        backgroundColor: [
-            "hsl(10, 79%, 65%)"
-        ],
-        hoverBackgroundColor: [
-            "hsl(10, 66%, 73%)"
-        ],
-        borderRadius: [
-            "3"
-        ]
-    }]
-}
+fetch("./data.json")
+    .then(res => res.json())
+    .then(spendingData => {
 
-const config = {
-    type: "bar",
-    data: data,
-    options: {
-        scales: {
-            y: {
-                grid: {
-                    display: false,
-                    drawBorder: false
-                }
-            },
-            x: {
-                grid: {
-                    display: false,
-                    drawBorder: false
-                }
-            },
+        // console.log(spendingData)
 
-        },
-        plugins: {
-            legend: {
-                display: false
-            },
-            background: {
-                display: false
+        // Put data in separated array
+        for (let e of spendingData) {
+            const day = e.day
+            labels.push(day)
+        }
+        for (let e of spendingData) {
+            const amount = e.amount
+            spending.push(amount)
+        }
+
+        // Different bgColor for highest value
+        let max = Math.max(...spending)
+        // console.log(max)
+        for (let e of spending) {
+            if (e === max) {
+                backgroundcolor.push("hsl(186, 34%, 60%)")
+                hoverbackgroundcolor.push("hsl(187, 49%, 80%")
+            } else {
+                backgroundcolor.push("hsl(10, 79%, 65%)")
+                hoverbackgroundcolor.push("hsl(10, 100%, 76%)")
             }
         }
-    },
-}
 
-const spendingChart = new Chart(
-    document.getElementById("spendingChart"), config
-)
+        // Chart
+        const data = {
+            labels: labels,
+            datasets: [{
+                data: spending,
+                backgroundColor: backgroundcolor,
+                hoverBackgroundColor: hoverbackgroundcolor,
+                borderRadius: [
+                    "5"
+                ]
+            }]
+        }
+
+        const config = {
+            type: "bar",
+            data: data,
+            options: {
+                scales: {
+                    y: {
+                        grid: {
+                            display: false,
+                            drawBorder: false
+                        }
+                    },
+                    x: {
+                        grid: {
+                            display: false,
+                            drawBorder: false
+                        }
+                    },
+
+                },
+                plugins: {
+                    legend: {
+                        display: false
+                    },
+                    background: {
+                        display: false
+                    }
+                }
+            },
+        }
+
+        const spendingChart = new Chart(
+            document.getElementById("spendingChart"), config
+        )
+
+
+    })
+
+// console.log(labels)
+// console.log(spending)
